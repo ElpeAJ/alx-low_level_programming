14b37d3ecbc04681106c3a0f5b99ecdb46e4985e
ElpeAJ <elpediaarthurjunior@gmail.com>
2023-05-22 14:02:08 -0700

    A function that creates a new dog. Task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,6 +10,7 @@
 int _strlen(char *s)
 {
 	int i;
+
 	i = 0;
 
 	while (s[i] != '\0')
@@ -28,9 +29,10 @@ int _strlen(char *s)
  * Return: the pointer to dest
  */
 
-char *_strcpy(car *dest, char *src)
+char *_strcpy(char *dest, char *src)
 {
 	int len, i;
+
 	len = 0;
 
 	while (src[len] != '\0')
@@ -80,6 +82,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 	_strcpy(dog->name, name);
 	_strcpy(dog->owner, owner);
 	dog->age = age;
-	
+
 	return (dog);
 }
