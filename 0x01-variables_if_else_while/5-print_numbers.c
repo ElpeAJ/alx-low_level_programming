d92b6ab2c48b4204590e16dd120c30b2d07df5db
ElpeAJ <elpediaarthurjunior@gmail.com>
2023-06-15 10:10:53 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - function prototype
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		i++;
+		printf("%d\n", i);
+	}
+
+	return (0);
+}
