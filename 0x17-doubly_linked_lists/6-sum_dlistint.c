7cf4db58864d1fb3da41a94f0f7634e811e91386
ElpeAJ <elpediaarthurjunior@gmail.com>
2023-07-21 13:13:46 -0700

    Task 6

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - Function that returns the sum of n
+ * @dlistint_t: list which it's data is to be summed
+ * @head: Pointer to the list
+ *
+ * Return: sum of n, otherwise 0 for empty list
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	dlistint_t *present = NULL;
+	int sum = 0;
+
+	present = head;
+	while (present)
+	{
+		sum += present->n;
+		present = present->next;
+	}
+
+	return (sum);
+
+}
