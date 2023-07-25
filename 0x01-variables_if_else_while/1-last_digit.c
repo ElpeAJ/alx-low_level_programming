c7a40b10d11be88577458440eaf93e243c73639b
ElpeAJ <elpediaarthurjunior@gmail.com>
2023-07-25 15:03:55 -0700

    Task 1 correction

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -28,7 +28,7 @@ int main(void)
 		printf("Last digit of %d is %d", n, last_digit);
 		printf(" and is 0.\n");
 	}
-	else if (last_digit < 6 && n != 0)
+	else if (last_digit < 6 && last_digit != 0)
 	{
 		printf("Last digit of %d is %d and", n, last_digit);
 		printf(" is less than 6 and not 0.\n");
